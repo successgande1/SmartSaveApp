05ba41f951435e06aa4f07c1cc7084f353860335
Apollos Gande <successgande@hotmail.com>
2023-08-19 11:14:26 +0100

    Setup SmartSaveProjct, installed accounts and savings accounts, installed packages, setup database and settins file

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect,  get_object_or_404
+from django.contrib.auth import logout
+from django.core.paginator import Paginator
+from django.contrib import messages 
+import json
+
+# 404 Error Page.
+def custom_page_not_found(request, exception=None):
+    return render(request, 'accounts/404.html', status=404)
